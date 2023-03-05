e42119bea324a5f7101fd81a596e02e77c12e490
Martin C <martin.collahua@gmail.com>
2023-03-05 09:24:58 -0800

    Raycast

---
+++ HorrorProject/Assets/Scripts/Gun.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float range = 100f;
+    public Camera fpscam;
+    public GameObject impactEffects;    
+     public ParticleSystem FlashEffect;      
+     public ParticleSystem CasquillosEffects;
+     public AudioSource _audioSource;
+     public AudioClip _clipDisparo;
+     public AudioClip _clip_Hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Disparar();
+        }
+    }
+
+    void Disparar()
+    {
+      
+        RaycastHit hit;
+        AudioPlay(_clipDisparo);
+          FlashEffect.Play();
+        if(Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+        {
+            if(hit.transform.tag == "Enemy")
+            {
+                hit.transform.gameObject.GetComponent<Enemy>().vida -=10f;
+                AudioPlay(_clip_Hit);
+                Debug.Log("entre");
+                GameObject impactEnemy = Instantiate(impactEffects, hit.point,Quaternion.LookRotation(hit.normal));
+                   Debug.Log("entre1");
+                Destroy(impactEnemy,2f);
+            }
+        }
+    }
+
+    void AudioPlay(AudioClip _clipTest)
+    {
+        _audioSource.clip = _clipTest;
+        _audioSource.Play();
+    }
+}
---
+++ HorrorProject/Assets/Scripts/PlayerFPS.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class PlayerFPS : MonoBehaviour
+
+{
+
+//Player Movement
+
+public float velocidadMovimiento = 3f;
+
+// public Rigidbody rb;
+
+//Player Look Rotation
+
+public Vector2 sensibilidadMouse;
+public Transform Camera;
+public Transform playerBody;
+
+private Vector3 rotate;
+
+    void Start()
+
+    {
+    BloqueoCusor();
+    }
+
+    void Update()
+
+    {
+
+        Move();
+
+        MouseLook();
+        
+
+    }
+
+    void Move()
+
+    {
+
+        float hor = Input.GetAxis("Horizontal");
+
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 inputPlayer = new Vector3(hor, 0, ver);
+
+        transform.Translate(new Vector3(hor, 0, ver) * velocidadMovimiento * Time.deltaTime);
+
+        // rb.AddForce(inputPlayer * velocidadMovimiento * Time.deltaTime);
+
+    }
+
+void MouseLook()
+
+    {
+
+     float hor = Input.GetAxis("Mouse X");
+     float ver = Input.GetAxis("Mouse Y");
+     transform.Rotate(Vector3.up*hor* sensibilidadMouse.x);
+    //   Camera.Rotate(Vector3.left*ver*sensibilidadMouse.y);
+    float angle = (Camera.localEulerAngles.x - ver*sensibilidadMouse.y+360)%360; // aqui se encarga que el angulo este entre 0 y 360
+    if(angle>180) {angle -=360;} // hacemos que el angulo este entre -180 a 180
+    angle= Mathf.Clamp(angle,-80,80);
+    Camera.localEulerAngles = Vector3.right*angle;
+
+    }
+
+
+public void BloqueoCusor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+}
